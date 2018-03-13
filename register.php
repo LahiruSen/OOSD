da55ab3c4189e38b8a8a6e8978e9c30fba456d64
DESKTOP-TFH0LVT\BlackLion <blacklionsweer@gmail.com>
2018-03-13 13:43:54 +0530

    completed basic registration

--- register.php
+++ register.php
@@ -3,6 +3,7 @@
    and sends account confirmation email message
  */
 
+
 // Set session variables to be used on profile.php page
 $_SESSION['email'] = $_POST['email'];
 $_SESSION['first_name'] = $_POST['firstname'];
@@ -10,26 +11,27 @@ $_SESSION['last_name'] = $_POST['lastname'];
 
 // Escape all $_POST variables to protect against SQL injections
 $first_name = $mysqli->escape_string($_POST['firstname']);
+$types = $mysqli->escape_string($_POST['types']);
 $last_name = $mysqli->escape_string($_POST['lastname']);
 $email = $mysqli->escape_string($_POST['email']);
 $password = $mysqli->escape_string(password_hash($_POST['password'], PASSWORD_BCRYPT));
 $hash = $mysqli->escape_string( md5( rand(0,1000) ) );
-      
+
 // Check if user with that email already exists
 $result = $mysqli->query("SELECT * FROM users WHERE email='$email'") or die($mysqli->error());
 
 // We know user email exists if the rows returned are more than 0
 if ( $result->num_rows > 0 ) {
-    
+
     $_SESSION['message'] = 'User with this email already exists!';
     header("location: error.php");
-    
+
 }
 else { // Email doesn't already exist in a database, proceed...
 
     // active is 0 by DEFAULT (no need to include it here)
-    $sql = "INSERT INTO users (first_name, last_name, email, password, hash) " 
-            . "VALUES ('$first_name','$last_name','$email','$password', '$hash')";
+    $sql = "INSERT INTO users (first_name, last_name, email, password, hash, types) "
+        . "VALUES ('$first_name','$last_name','$email','$password', '$hash', '$types')";
 
     // Add user to the database
     if ( $mysqli->query($sql) ){
@@ -37,8 +39,8 @@ else { // Email doesn't already exist in a database, proceed...
         $_SESSION['active'] = 0; //0 until user activates their account with verify.php
         $_SESSION['logged_in'] = true; // So we know the user has logged in
         $_SESSION['message'] =
-                
-                 "Confirmation link has been sent to $email, please verify
+
+            "Confirmation link has been sent to $email, please verify
                  your account by clicking on the link in the message!";
 
         // Send registration confirmation link (verify.php)
@@ -60,7 +62,7 @@ else { // Email doesn't already exist in a database, proceed...
         mail( $to, $subject, $message_body , $headers );
 
 
-        header("location: profile.php"); 
+        header("location: profile.php");
 
     }
 
