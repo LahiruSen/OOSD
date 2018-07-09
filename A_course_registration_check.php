8a759c922d8f52b58cae74c1def9bc2536658540
AathmanT <aathmant@gmail.com>
2018-07-09 13:21:37 +0530

    Fixed checking parts

--- A_course_registration_check.php
+++ A_course_registration_check.php
@@ -4,6 +4,9 @@ session_start();
 
 if(!empty($_POST)){
 
+    if($results2=mysqli->query("select id from course_registration where registration_number='{$_P}'")){
+
+    }
    if( $results=$mysqli->query("insert into course_registration (registration_number,is_approved,date_of_create,date_of_update,course_id) values ('{$_POST['reg_num']}',0,NOW(),NOW(),'{$_POST['course_id']}')")){
        if($results->affected_rows!=0){
            $_SESSION['message'] = "Something went wrong!";
