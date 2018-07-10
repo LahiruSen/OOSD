cf569a6563cf76b7ff876807dbd124172db55395
LAHIRU-HP\Lahiru <lahiru.16@cse.mrt.ac.lk>
2018-07-10 12:03:44 +0530

    Added about section

--- l_leave_submission.php
+++ l_leave_submission.php
@@ -217,18 +217,16 @@ if ($_SESSION['logged_in'] != 1) {
             <hr class="star-light mb-5">
             <div class="row">
                 <div class="col-lg-4 ml-auto">
-                    <p class="lead">Basic introduction about the web site goes here! {description left]</p>
+                    <h4>Our Vision</h4>
+                    <p class="lead">To become the most efficient training providing organization effectively contributing to achieve prosperity in Sri Lanka through Human Resource Development.</p>
                 </div>
                 <div class="col-lg-4 mr-auto">
-                    <p class="lead">Basic introduction about the web site goes here! {description right</p>
+                    <h4>Our Mission</h4>
+                    <p class="lead">Providing vocational and Technical Training for youth, to acquire employable skills through well formulated skills programs with highest professional Standards to meet the skilled manpower requirement in the industry.</p>
                 </div>
             </div>
-            <div class="text-center mt-4">
-                <a class="btn btn-xl btn-outline-light" href="#">
-                    <i class="fa fa-info mr-2"></i>
-                    Read More
-                </a>
-            </div>
+
+
         </div>
     </section>
 
